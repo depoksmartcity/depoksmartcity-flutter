f9e1af4b7e1ad451aa6db3565ff30ae9b7f9bab4
ariefbraja <marief.braja@gmail.com>
2022-12-06 23:09:35 +0700

    login flutter

---
+++ lib/drawer/drawer.dart
@@ -0,0 +1,47 @@
+import 'package:depoksmartcity/main.dart';
+import 'package:depoksmartcity/page/auth/login.dart';
+import 'package:flutter/material.dart';
+
+class DrawerClass extends StatefulWidget {
+  const DrawerClass({Key? key}) : super(key: key);
+
+  @override
+  _DrawerClassState createState() => _DrawerClassState();
+}
+
+class _DrawerClassState extends State<DrawerClass> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          // Untuk yang menu counter_7
+          ListTile(
+            title: const Text("counter_7"),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Flutter Demo Home Page')),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text("Login"),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
